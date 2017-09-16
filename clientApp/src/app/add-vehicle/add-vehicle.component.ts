188f3d4d1ce320a53a0213def0a49b03fc703d11
d400800 <d400801@gmail.com>
2017-09-16 14:33:22 +0300

    adding checkpost select, adding datepicker css

--- clientApp/src/app/add-vehicle/add-vehicle.component.ts
+++ clientApp/src/app/add-vehicle/add-vehicle.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PassengerDriver, FormData } from '../classes/formData';
+import { MainService } from '../services/main.service';
+import { CheckpostPanel } from '../classes/checkpost-panel';
 
 @Component({
   selector: 'app-add-vehicle',
@@ -9,27 +11,46 @@ import { PassengerDriver, FormData } from '../classes/formData';
 })
 export class AddVehicleComponent implements OnInit {
 
-	name: string;
+	checkpostRouteName: string;
 	formData: FormData;
 	he: any;
 	myValue: boolean = true;
+	checkpostPanels: CheckpostPanel[];
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private mainService: MainService) { }
 
 	ngOnInit() {
-	  this.name = this.activatedRoute.snapshot.url[1].path;
-  	this.formData = new FormData("", undefined, "", "", [], new PassengerDriver(), "", "", this.name);
+	  this.checkpostRouteName = this.activatedRoute.snapshot.url[1].path;
+		this.getCheckpostPanels();
+  	this.formData = new FormData("", undefined, "", "", [], new PassengerDriver(), "", "", new CheckpostPanel());
 
 	  this.he = {
         firstDayOfWeek: 0,
         dayNames: ["יום ראשון", "יום שני", "יום שלישי","יום רביעי", "יום ימישי", "יום שישי", "יום שבת"],
         dayNamesShort: ["א", "ב", "ג", "ד", "ה", "ו", "ש"],
-        monthNames: [ "ינואר","פברואר","מרץ","אפריל","מאי","יוני","יולי","אוגוסט","ספטמבר","אוקטובר","נובמבר","דצמבר" ],
-        monthNamesShort: [ "ינואר", "פברואר", "מרץ", "אפריל", "מאי", "יוני","יולי", "אוגוסט", "ספטמבר", "אוקטובר", "נובמבר", "דצמבר" ]
+        monthNames: ["ינואר", "פברואר", "מרץ", "אפריל", "מאי", "יוני", "יולי", "אוגוסט", "ספטמבר", "אוקטובר", "נובמבר", "דצמבר"],
+        monthNamesShort: ["ינואר", "פברואר", "מרץ", "אפריל", "מאי", "יוני","יולי", "אוגוסט", "ספטמבר", "אוקטובר", "נובמבר", "דצמבר"]
     };
 	}
 
-	onSubmit(driverPassport, driverPhone) {
+	getCheckpostPanels(): void {
+  	this.mainService.getCheckpostPanels().then(checkpostPanels => {
+  		this.checkpostPanels = checkpostPanels;
+  		
+  		this.checkpostPanels.forEach( (checkpostPanel) => {
+			  if(checkpostPanel.name == this.checkpostRouteName) {
+			  	this.formData.checkpost = checkpostPanel;
+			  } 
+			});
+
+  	});
+  }
+
+  compareFn(c1, c2): boolean {
+    return c1 && c2 ? c1.id === c2.id : c1 === c2;
+	}
+
+	onSubmit(driverPassport, driverPhone, checkpostName) {
 		
 		// we allow the form to be submitted if either driver's passport or driver's phone is invalid,
 		// however we do not want an invalid value to be transfered to backend, so we check the values
